1193e4ffde6a8e90516c13829d32808c9fbb9035
seunbayo <seunbayo83@gmail.com>
2022-11-10 21:25:46 +0100

    twitter dapp

---
+++ src/constant.js
@@ -0,0 +1,24 @@
+export const APP_CONSTANTS = {
+  ALCHEMY_KEY: "<Quick_Node_Polygon_WSS_Provider>",
+
+  CONTRACT_ADDRESS: "0xBe6d970928F769c091694c81Df22da87a47fbAed",
+
+  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+
+  CLIENT_ID: "b4llXMDtEIzoDFq5mR6R5c1ZHlOmoVYQ",
+
+  RPC_TARGET:
+    "https://late-dimensional-morning.matic-testnet.discover.quiknode.pro/e246948bd7da6cdcfd90697e9a8712e9c0b28dc6/",
+
+  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+
+  ADAPTER_TWITTER_CLIENT_VERIFIER: "seunTwitterDapp",
+
+  ADAPTER_TWITTER_CLIENT_ID: "b4llXMDtEIzoDFq5mR6R5c1ZHlOmoVYQ",
+
+  AUTH0_DOMAIN: "dev-p0xkjsf656nrqy8u.us.auth0.com",
+
+  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+
+  REACT_APP_REFRESH_TIMER: 30,
+};
---
+++ src/constants.js
@@ -0,0 +1,23 @@
+export const APP_CONSTANTS = {
+  ALCHEMY_KEY: "<Quick_Node_Polygon_WSS_Provider>",
+
+  CONTRACT_ADDRESS: "<deployed_contract_address>",
+
+  OPENSEA_ASSETS_URL: "https://testnets.opensea.io/assets/mumbai",
+
+  CLIENT_ID: "<web3auth_client_id>",
+
+  RPC_TARGET: "<Quick_Node_Polygon_HTTP_Provider>",
+
+  APP_LOGO: "https://i.ibb.co/2gmPw9f/twitter-Dapp-Logo.jpg",
+
+  ADAPTER_TWITTER_CLIENT_VERIFIER: "<web3auth_custom_auth_verifier_name>",
+
+  ADAPTER_TWITTER_CLIENT_ID: "<auth0_client_id>",
+
+  AUTH0_DOMAIN: "<auth0_domain>",
+
+  BIRD_IMAGE: "https://i.ibb.co/42WNxRN/reshot-icon-bird-TL39-D5-P8-YH.png",
+
+  REACT_APP_REFRESH_TIMER: 30,
+};
